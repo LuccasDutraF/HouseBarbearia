01cc333447991f857a6f70b0a276678646a95d87
LuccasDutraF <luccas.ferreyro@gmail.com>
2025-08-28 01:29:41 -0300

    Mudança de responsividade e ajustando menu lateral

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const hero2 = document.querySelector('.hero-wallpaper2')
 const localizaredes = document.querySelector('.localiza-redes')
 const localizacao = document.querySelector('.localizacao')
 const footer = document.querySelector('footer')
-const body = document.querySelector('.body')
+const body = document.querySelector('body')
 const cortes = document.querySelector('.cortes')
 const feedback = document.querySelector('.feedback')
 const barbeiros = document.querySelector('.barbeiros')
-const barbeirosremovido = document.getElementById('barbeiroremovido')
+const barbeirosremovido1 = document.getElementById('barbeiroremovido1')
+const barbeirosremovido2 = document.getElementById('barbeiroremovido2')
+const barbeiroimg = document.querySelector('.barbeiroimg')
+const barbeiroimg2 = document.querySelector('.barbeiroimg2')
+const corte1 = document.querySelector('.corte1')
+const corte2 = document.querySelector('.corte2')
+const corte3 = document.querySelector('.corte3')   
 
 function mostrarmenumobile(){
     menumobile.style.transform = 'translateX(0)'
@@ -19,15 +25,32 @@ function mostrarmenumobile(){
     localizacao.style.display = 'none'
     footer.style.display = 'none'
     body.style.height = '100vh'
-    cortes.style.display = 'none'
-    feedback.style.display = 'none'
-    barbeiros.style.display = 'none'
-    barbeirosremovido.style.display = 'none'
     
 
     if(window.innerWidth >= 768){
         about.style.display = 'flex'
     }
+}
+
+function mostrarmenumobilecortes(){
+    menumobile.style.transform = 'translateX(0)'
+    menumobile.style.display = 'flex'
+    feedback.style.display = 'none'
+    footer.style.display = 'none'
+    corte1.style.display = 'none'
+    corte2.style.display = 'none'
+    corte3.style.display = 'none'
+    cortes.style.padding = '50px 20px 0px 20px'
+}
+
+function mostrarmenumobilebarbeiros(){
+    menumobile.style.transform = 'translateX(0)'
+    menumobile.style.display = 'flex'
+    barbeirosremovido1.style.display = 'none'
+    barbeirosremovido2.style.display = 'none'
+    footer.style.display = 'none'
+    barbeiroimg.style.width = '90%'
+    barbeiroimg2.style.width = '90%'
 }
 
 function tirarmenumobile(){
@@ -41,6 +64,29 @@ function tirarmenumobile(){
     body.style.height = '100%'
     cortes.style.display = 'flex'
     feedback.style.display = 'flex'
-    barbeiros.style.display = 'flex'
-    barbeirosremovido.style.display = 'flex'
+}
+
+function tirarmenumobilecortes(){
+    menumobile.style.transform = 'translateX(500px)'
+    menumobile.style.display = 'none'
+    footer.style.display = 'block'
+    body.style.height = '100%'
+    feedback.style.display = 'flex'
+    footer.style.display = 'block'
+    corte1.style.display = 'block'
+    corte2.style.display = 'block'
+    corte3.style.display = 'block'
+    cortes.style.padding = '50px 20px 60px 20px'
+}
+
+function tirarmenumobilebarbeiros(){
+    menumobile.style.transform = 'translateX(500px)'
+    menumobile.style.display = 'none'
+    barbeiros.style.display = 'block'
+    barbeirosremovido1.style.display = 'block'
+    barbeirosremovido2.style.display = 'block'
+    barbeiroimg.style.width = '100%'
+    barbeiroimg2.style.width = '100%'
+    footer.style.display = 'block'
+    body.style.height = '100%'
 }
